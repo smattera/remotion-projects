3fa8a1434d3446b47f513fb7aa65db59a3b74c03
smattera <smattera@hotmail.com>
2026-07-04 19:04:50 -0500

    Quick Start video: slower typing, read pauses, fade/shrink transition, final logo

---
+++ quick-start/src/Root.tsx
@@ -0,0 +1,15 @@
+import { Composition } from "remotion";
+import { QuickStart } from "./QuickStart";
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <Composition
+      id="QuickStart"
+      component={QuickStart}
+      durationInFrames={390}
+      fps={30}
+      width={1920}
+      height={1080}
+    />
+  );
+};
